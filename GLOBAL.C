15b466a06896f46dec075ce932a1eeee8a7b952f
sharvapatel12 <sharvapatel12@gmail.com>
2026-09-10 15:48:43 +0530

    Add files via upload

---
+++ GLOBAL.C
@@ -0,0 +1,17 @@
+//global variable demo
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int i;
+	clrscr();
+	for(i=1;i<=20;i++)
+
+	{
+		printf("\a Thank you");
+		delay("1000");
+
+	}
+	getch();
+}
