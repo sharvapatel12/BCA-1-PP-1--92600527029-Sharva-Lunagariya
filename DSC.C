d610ce9be79ff8af9ded28882e4434eea5e36c95
sharvapatel12 <sharvapatel12@gmail.com>
2026-09-11 11:07:27 +0530

    Add files via upload

---
+++ DSC.C
@@ -0,0 +1,16 @@
+//defining symbolic constants
+#include<stdio.h>
+#include<conio.h>
+
+#define PI 3.14
+#define pf printf
+
+void main()
+{
+	float r=2.5,a;
+	clrscr();
+	a= PI * r * r ;
+	pf("\n area is : %f",a);
+	getch();
+
+}
